c9b2c94f0e94e6ca857d7172a57ae9e6dd662f51
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2018-09-13 20:22:51 +0200

    Basic working version

--- src/services/config.ts
+++ src/services/config.ts
@@ -2,6 +2,6 @@ export const getConfig = (): Config => {
   return {
     MAPBOX_API_KEY:
       'pk.eyJ1Ijoia3J6eXN6dG9menVyYXciLCJhIjoiY2pramxyeWRsMGJ2NTNycDh3Y2lkZ2ttZyJ9.atnnfyXMOY2n1vxfJnNJMQ',
-    MAPBOX_MAP_ID: 'mapbox.streets',
+    MAPBOX_MAP_STYLE: 'mapbox://styles/mapbox/streets-v9',
   };
 };
--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -1,4 +1,8 @@
 declare interface Config {
   MAPBOX_API_KEY: string;
-  MAPBOX_MAP_ID: string;
+  MAPBOX_MAP_STYLE: string;
+}
+
+declare interface Window {
+  mapboxgl: any;
 }
